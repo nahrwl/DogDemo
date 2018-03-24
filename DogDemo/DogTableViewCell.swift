93ea460655d3346e52c3dcead23a37d05b38a474
Nathan Wallace <techietotoro@gmail.com>
2018-03-23 21:55:40 -0700

    Adds Pods and sets up basic table view

---
+++ DogDemo/DogTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DogTableViewCell.swift
+//  DogDemo
+//
+//  Created by Nathan Wallace on 3/23/18.
+//  Copyright © 2018 Nathan Wallace. All rights reserved.
+//
+
+import UIKit
+
+class DogTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var dogTitle: UILabel!
+
+}
